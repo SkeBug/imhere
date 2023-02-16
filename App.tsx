ba2404fbe84893398ba54b34d7dea8df3fa06477
Evandro Silva <evandroagostinho.silva@equilibrium.co.ao>
2023-02-16 18:04:16 +0100

    using style in-line

--- App.tsx
+++ App.tsx
@@ -2,9 +2,27 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Nome do evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Quinta, 16 de Fevereiro de 2023
+      </Text>
+
     </View>
   )
 }
