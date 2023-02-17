33d370ef87f0cb52e5a4e121eb95f9a359d995e5
Evandro Silva <evandroagostinho.silva@equilibrium.co.ao>
2023-02-17 17:12:08 +0100

    added add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, TextInput, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('clique')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -17,6 +22,12 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
 
     </View>
   )
